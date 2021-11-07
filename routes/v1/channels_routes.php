1edb85a519d9aa87fdc96f4189549988813ca900
unknown <darius134966@gmail.com>
2021-11-07 09:48:57 +0330

    update channel tests for roles and permissions

--- routes/v1/channels_routes.php
+++ routes/v1/channels_routes.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Api\v1\Channel\ChannelController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('/channel')->middleware('can:channel management')->group(function () {
+Route::prefix('/channel')->group(function () {
 
 
     Route::get('/all', [
@@ -16,19 +16,24 @@ Route::prefix('/channel')->middleware('can:channel management')->group(function 
 
     ])->name('channel.all');
 
-    Route::post('/create', [
 
-        ChannelController::class, 'createNewChannel'
 
-    ])->name('channel.create');
-    Route::put('/update', [
+    Route::middleware('can:channel management')->group(function () {
 
-        ChannelController::class, 'updateChannel'
+        Route::post('/create', [
 
-    ])->name('channel.update');
-    Route::delete('/delete', [
+            ChannelController::class, 'createNewChannel'
 
-        ChannelController::class, 'deleteChannel'
+        ])->name('channel.create');
+        Route::put('/update', [
 
-    ])->name('channel.delete');
+            ChannelController::class, 'updateChannel'
+
+        ])->name('channel.update');
+        Route::delete('/delete', [
+
+            ChannelController::class, 'deleteChannel'
+
+        ])->name('channel.delete');
+    });
 });
--- tests/Unit/ChannelTest.php
+++ tests/Unit/ChannelTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Unit;
 
 use App\Channel;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 
 class ChannelTest extends TestCase
@@ -11,23 +14,69 @@ class ChannelTest extends TestCase
 
     use RefreshDatabase;
 
+
+    public function registerRolesAndPemissions()
+    {
+        $roleInDatabase = Role::where('name', config('permission.default_roles')[0]);
+
+        if ($roleInDatabase->count() > 1) {
+            foreach (config('permission.default_roles') as $role) {
+
+                Role::create([
+
+                    'name' => $role
+
+                ]);
+            }
+        }
+
+        //permissions creating
+
+        $permissionInDatabase = Permission::where('name', config('permission.default_permissions')[0]);
+
+        if ($permissionInDatabase->count() > 1) {
+            foreach (config('permission.default_permissions') as $permission) {
+
+                Permission::create([
+
+                    'name' => $permission
+
+                ]);
+            }
+        }
+    }
+
+
     public function test_all_channels_should_be_accessible()
     {
-       $response = $this->get(route('channel.all'));
+        $response = $this->get(route('channel.all'));
 
-       $response->assertStatus(200);
+        $response->assertStatus(200);
     }
 
     public function test_channel_should_be_validated()
     {
-        $response = $this->postJson(route('channel.create'), []);
+
+        $this->registerRolesAndPemissions();
+
+        $user = factory(User::class)->create();
+        $user->givePermissionTo('channel management');
+
+        $response = $this->actingAs($user)->postJson(route('channel.create'), []);
 
         $response->assertStatus(422);
     }
 
     public function test_channel_can_be_created()
     {
-        $response = $this->postJson(route('channel.create'), [
+
+        $this->registerRolesAndPemissions();
+
+        $user = factory(User::class)->create();
+        $user->givePermissionTo('channel management');
+
+
+        $response = $this->actingAs($user)->postJson(route('channel.create'), [
 
             'name' => 'laravel'
 
@@ -39,7 +88,12 @@ class ChannelTest extends TestCase
 
     public function test_channel_update_should_be_validated()
     {
-        $response = $this->json('PUT', route('channel.update'), []);
+        $this->registerRolesAndPemissions();
+
+        $user = factory(User::class)->create();
+        $user->givePermissionTo('channel management');
+
+        $response = $this->actingAs($user)->json('PUT', route('channel.update'), []);
 
         $response->assertStatus(422);
     }
@@ -47,9 +101,14 @@ class ChannelTest extends TestCase
     public function test_channel_update()
     {
 
+        $this->registerRolesAndPemissions();
+
+        $user = factory(User::class)->create();
+        $user->givePermissionTo('channel management');
+
         $channel = factory(Channel::class)->create();
 
-        $response = $this->json('PUT', route('channel.update'), [
+        $response = $this->actingAs($user)->json('PUT', route('channel.update'), [
 
             'id' => $channel->id,
             'name' => 'vuejs'
@@ -63,16 +122,26 @@ class ChannelTest extends TestCase
 
     public function test_channel_delete_should_be_validated()
     {
-        $response = $this->json('DELETE', route('channel.delete'));
+        $this->registerRolesAndPemissions();
+
+        $user = factory(User::class)->create();
+        $user->givePermissionTo('channel management');
+
+        $response = $this->actingAs($user)->json('DELETE', route('channel.delete'));
 
         $response->assertStatus(422);
     }
 
     public function test_delete_channel()
     {
+        $this->registerRolesAndPemissions();
+
+        $user = factory(User::class)->create();
+        $user->givePermissionTo('channel management');
+
         $channel = factory(Channel::class)->create();
 
-        $response = $this->json('DELETE', route('channel.delete'), [
+        $response = $this->actingAs($user)->json('DELETE', route('channel.delete'), [
 
             'id' => $channel->id
 
@@ -80,5 +149,4 @@ class ChannelTest extends TestCase
 
         $response->assertStatus(200);
     }
-
 }
